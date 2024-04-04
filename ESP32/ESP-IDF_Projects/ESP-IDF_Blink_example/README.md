1f864f899f87dd9f39e4ca8d9bcb3367496cf295
AbhithlalC <iamabithlalc@gmail.com>
2024-04-05 00:35:14 +0530

    Adding LED blinking
    Adding Led blinking in esp32 using esp-idf.

---
+++ ESP32/ESP-IDF_Projects/ESP-IDF_Blink_example/README.md
@@ -0,0 +1,32 @@
+# ESP32 Blink Example using ESP-IDF
+
+## Overview
+
+This repository contains a simple "Blink" example for ESP32 using ESP-IDF. The project demonstrates how to blink an LED connected to a GPIO pin using the ESP-IDF framework. The LED blinks with a 2-second interval.
+
+## Requirements
+
+- ESP-IDF development environment
+
+## Project Details
+
+- **File**: blink_example.c
+- **Author**: ABHITHLAL C
+- **MCU**: ESP32
+- **GPIO Pin**: 2 (configurable)
+
+## Usage
+
+To use this project:
+
+1. Set up the ESP-IDF development environment.
+2. Clone this repository to your local machine.
+3. Compile the project using the ESP-IDF build system.
+4. Flash the compiled firmware to your ESP32.
+5. Observe the LED connected to GPIO pin 2 blinking with a 2-second interval.
+
+## File Structure
+
+- `blink.c`: Main source code file containing the "Blink" example.
+- `CMakeLists.txt`: CMake configuration file for building the project.
+- `sdkconfig`: Configuration settings for the ESP-IDF SDK.
---
+++ ESP32/ESP-IDF_Projects/ESP-IDF_Blink_example/main/blink_example.c
@@ -0,0 +1,46 @@
+/**
+ * @file blink_example.c
+ *
+ * @brief Blink example for ESP32 using ESP-IDF.
+ *
+ * This is a simple example demonstrating how to blink an LED connected to a GPIO pin
+ * using the ESP-IDF framework. The LED blinks with a 2-second interval.
+ *
+ * Written by: 
+ */
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define LED_PIN 2
+
+void app_main() 
+{
+    // Set GPIO pin direction to OUTPUT
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    // Infinite loop to toggle the LED
+    while (1) 
+    {
+        // Log message indicating LED is turning ON
+        ESP_LOGI("LED","LED ON!!!");
+        
+        // Turn ON the LED by setting GPIO pin level to HIGH
+        gpio_set_level(LED_PIN, 1);
+        
+        // Delay for 2 seconds
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        
+        // Log message indicating LED is turning OFF
+        ESP_LOGI("LED","LED OFF!!!");
+        
+        // Turn OFF the LED by setting GPIO pin level to LOW
+        gpio_set_level(LED_PIN, 0);
+        
+        // Delay for 2 seconds
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
+}
